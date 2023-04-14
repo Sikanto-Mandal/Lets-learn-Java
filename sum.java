e1a3a558d9b2ed4a978812e898d9c0a2a24f329f
Sikanto Mandal <sikantomandal@gmail.com>
2023-04-15 01:30:19 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers : ");
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        double sum = num1 + num2;
+        System.out.print ("Sum is = " + sum);
+    }
+    
+}
