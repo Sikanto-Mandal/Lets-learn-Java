7b50170afe8dbb4b3b06ac909d2dd91738afdcb3
Sikanto Mandal <pogo591998@gmail.com>
2023-04-21 00:32:13 +0530

    Function OverLoading

--- fuctionOverloading.java
+++ fuctionOverloading.java
@@ -6,15 +6,24 @@ public class fuctionOverloading {
         return a + b;
 
     }
-     // Function of sum three numbers-
+    //  // Function of sum three numbers-
 
-     public static int sum (int a, int b, int c){
-        return a + b + c;
-     }
+    //  public static int sum (int a, int b, int c){
+    //     return a + b + c;
+    //  }
+
+    //function of sum of float numbers
+
+    public static float sum(float a, float b){
+        return a + b;
+    }
     public static void main(String args[]){
 
         System.out.println(sum(3,2));
-        System.out.println(sum(3,4,5));
+
+        // System.out.println(sum(3,4,5));
+
+        System.out.println(sum(3.5f, 1.5f));
 
     }
     
