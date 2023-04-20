96535817a17535a474dfcd079c2ef91b71f919d9
Sikanto Mandal <pogo591998@gmail.com>
2023-04-20 07:47:28 +0530

    Function

---
+++ printFunHelloWorld.java
@@ -0,0 +1,17 @@
+
+public class printFunHelloWorld {
+
+    public static void printHelloWorld(){
+        System.out.println("HELLO WORLD");
+        System.out.println("Hello World");
+        System.out.println("HelloWorld");
+        return ;
+    }
+    public static void main(String args[]){
+        printHelloWorld();
+        
+
+    
+    }
+    
+}
