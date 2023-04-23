f54399b96856d8a70f3f121ee472bab22379a330
Sikanto Mandal <pogo591998@gmail.com>
2023-04-23 10:13:00 +0530

    isPrime

--- isPrime.java
+++ isPrime.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class isPrime {
     // for prime number
     public static boolean isPrime(int n){
-        if (n == 0){
+        if (n == 2){
             return true;
         }
         for(int i =2; i<= Math.sqrt(n); i++){
