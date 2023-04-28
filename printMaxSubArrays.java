a4985fecebb036cbe40c4113e40584ce2e3fae24
Sikanto Mandal <pogo591998@gmail.com>
2023-04-28 19:42:57 +0530

    Maximum of Sum Sub Arrays

---
+++ printMaxSubArrays.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class printMaxSubArrays {
+    public static void maxSumSubArrays(int arr[]){
+        int currSum =0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            int start =i;
+            for(int j =i; j<arr.length; j++){
+                int end =j;
+                currSum =0;
+                for(int k =start; k <= end; k++){
+                    currSum += arr[k];
+                }
+                System.out.print(currSum + " ");
+                if (maxSum <currSum){
+                    maxSum = currSum;
+                }
+                
+                
+            }
+        }
+        System.out.print("\nMaximum of Sum SubArray : " + maxSum);
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in );
+        int arr[] = new int[6];
+        System.out.println("Enter your Array Elements : ");
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+
+        }
+        System.out.println("Your Sum SubArrays : ");
+        maxSumSubArrays(arr);
+    }
+    
+}
