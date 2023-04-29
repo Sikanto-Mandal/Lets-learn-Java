d11d9ecd099f01821b306dfab21ff566770356c1
Sikanto Mandal <pogo591998@gmail.com>
2023-04-29 17:38:49 +0530

    Diagonal Matrix Sum

---
+++ diagonalMatrixSum.java
@@ -0,0 +1,31 @@
+public class diagonalMatrixSum {
+    public static int diagonalSum(int matrix[][]){
+        int sum =0;
+
+        // for(int i =0; i < matrix.length; i++){
+        //     for(int j =0; j<matrix[0].length; j++){
+        //         if(i == j){
+        //             sum+=matrix[i][j];
+        //         }
+        //         else if ( i + j == matrix.length-1 ){
+        //             sum+= matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        for(int i=0; i <matrix.length; i++){
+            //pd
+            sum+=matrix[i][i];
+            //sd
+            if(i != matrix.length-i-1)   // SD : (i+j)= (n-1) : (j= n-1-i)
+                sum+=matrix[i][matrix.length-i-1];
+
+        }
+        return sum;
+    }
+    public static void main(String args[]){
+        int matrix [][]= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+       System.out.print("Dianogal Sum is : "+diagonalSum(matrix));
+    }
+    
+}
