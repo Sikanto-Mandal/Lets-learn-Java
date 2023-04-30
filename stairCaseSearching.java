98ff9c09d19dd89a4bd8011499ab412652fbfc94
Sikanto Mandal <pogo591998@gmail.com>
2023-04-30 09:59:36 +0530

    Stair Case Searching

---
+++ stairCaseSearching.java
@@ -0,0 +1,27 @@
+public class stairCaseSearching {
+    public static boolean searchKey(int matrix[][], int key){
+        int row = matrix.length-1, col = 0;
+
+        while(row < matrix.length && col >= 0){
+            if (matrix[row][col] == key){
+                System.out.println("Found key at (" + row +"," +col + ")");
+                return true;
+            }
+            else if (key > matrix[row][col]){
+                col++;
+            }
+            else {
+                row--;
+            }
+        }
+        System.out.println("Key is not found!");
+        return false;
+    }
+    public static void main(String args[]){
+        int matrix [][]={{10, 20, 30, 40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        int key = 30;
+
+        searchKey(matrix,key);
+    }
+    
+}
