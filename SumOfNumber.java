7360c413eba7fb5fdd097ac0a52095e897415fa2
Sikanto Mandal <pogo591998@gmail.com>
2023-05-27 06:38:37 +0530

    SumOfNumbers

---
+++ SumOfNumber.java
@@ -0,0 +1,13 @@
+public class SumOfNumber {
+    public static void main (String args[]){
+        // int count = 0;
+        int n =5;
+        // for(int i = 1; i<= n; i++){
+            // count = count + i;
+            n = n*(n+1) /2;            
+        // }
+        System.out.println(n);
+        // System.out.print(count);
+    }
+    
+}
